421228f44d9e3f7599a723aee9e31c737fe48094
mayadi1 <admin@ayadios.com>
2016-11-06 11:40:40 -0800

    getting info from AWS

--- GraphView/ViewController.swift
+++ GraphView/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        self.getUserInfo()
         
         graphView = ScrollableGraphView(frame: chartView.frame)
         graphView = createDarkGraph(chartView.frame)
@@ -309,6 +310,73 @@ class ViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-  
+    
+    
+    func getUserInfo() {
+        
+        // Define server side script URL
+        let scriptUrl = "http://swiftdeveloperblog.com/my-http-get-example-script/"
+        // Add one parameter
+       // let urlWithParams = scriptUrl + "?userName=\(userNameValue!)"
+        // Create NSURL Ibject
+        let myUrl = NSURL(string: scriptUrl);
+        
+        // Creaste URL Request
+        let request = NSMutableURLRequest(url:myUrl! as URL);
+        
+        // Set request HTTP method to GET. It could be POST as well
+        request.httpMethod = "GET"
+        
+        // If needed you could add Authorization header value
+        // Add Basic Authorization
+        /*
+         let username = "myUserName"
+         let password = "myPassword"
+         let loginString = NSString(format: "%@:%@", username, password)
+         let loginData: NSData = loginString.dataUsingEncoding(NSUTF8StringEncoding)!
+         let base64LoginString = loginData.base64EncodedStringWithOptions(NSDataBase64EncodingOptions())
+         request.setValue(base64LoginString, forHTTPHeaderField: "Authorization")
+         */
+        
+        // Or it could be a single Authorization Token value
+        //request.addValue("Token token=884288bae150b9f2f68d8dc3a932071d", forHTTPHeaderField: "Authorization")
+        
+        // Excute HTTP Request
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            // Check for error
+            if error != nil
+            {
+                print("error=\(error)")
+                return
+            }
+            
+            // Print out response string
+            let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print("responseString = \(responseString)")
+            
+            
+            // Convert server json response to NSDictionary
+            do {
+                if let convertedJsonIntoDict = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
+                    
+                    // Print out dictionary
+                    print(convertedJsonIntoDict)
+                    
+                    // Get value by key
+//                    let firstNameValue = convertedJsonIntoDict["userName"] as? String
+//                    print(firstNameValue!)
+                    
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+        }
+        
+        task.resume()
+    }
+    
 }
 
