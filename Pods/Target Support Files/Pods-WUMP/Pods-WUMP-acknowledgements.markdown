defc7ff25230716fefc3c89fd054ee1b3e72e01e
mayadi1 <admin@ayadios.com>
2016-11-06 13:58:18 -0800

    Meter added

--- Pods/Target Support Files/Pods-WUMP/Pods-WUMP-acknowledgements.markdown
+++ Pods/Target Support Files/Pods-WUMP/Pods-WUMP-acknowledgements.markdown
@@ -1,11 +1,11 @@
 # Acknowledgements
 This application makes use of the following third party libraries:
 
-## ScrollableGraphView
+## LMGaugeView
 
 The MIT License (MIT)
 
-Copyright (c) 2016 Phillip
+Copyright (c) {{{2014}}} {{{LMinh}}}
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- Pods/Target Support Files/Pods-WUMP/Pods-WUMP-frameworks.sh
+++ Pods/Target Support Files/Pods-WUMP/Pods-WUMP-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/ScrollableGraphView/ScrollableGraphView.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/LMGaugeView/LMGaugeView.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/ScrollableGraphView/ScrollableGraphView.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/LMGaugeView/LMGaugeView.framework"
 fi
